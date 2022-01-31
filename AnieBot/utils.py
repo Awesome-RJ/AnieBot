e94906242078678d263fe92471b901f08b33de5e
丂ﾑﾶﾶƳ <89745675+FriDayXD@users.noreply.github.com>
2022-01-31 10:09:39 +0530

    Create utils.py

---
+++ AnieBot/utils.py
@@ -0,0 +1,93 @@
+import glob
+import logging
+import sys
+from pathlib import Path
+
+from telethon import events
+
+from . import OWNER_ID, tbot
+
+
+def Abot(**args):
+    pattern = args.get("pattern", None)
+    r_pattern = r"^[/?!+]"
+    if pattern is not None and not pattern.startswith("(?i)"):
+        args["pattern"] = "(?i)" + pattern
+    args["pattern"] = pattern.replace("^/", r_pattern, 1)
+
+    def decorator(func):
+        async def wrapper(check):
+            if check.sender_id and check.sender_id != OWNER_ID:
+                pass
+            try:
+                await func(check)
+            except BaseException:
+                return
+            else:
+                pass
+
+        tbot.add_event_handler(wrapper, events.NewMessage(**args))
+        return wrapper
+
+    return decorator
+
+
+def Cquery(**args):
+    pattern = args.get("pattern", None)
+
+    if pattern is not None and not pattern.startswith("(?i)"):
+        args["pattern"] = "(?i)" + pattern
+
+    def decorator(func):
+        tbot.add_event_handler(func, events.InlineQuery(**args))
+        return func
+
+    return decorator
+
+
+def Sammy(**args):
+    def decorator(func):
+        tbot.add_event_handler(func, events.CallbackQuery(**args))
+        return func
+
+    return decorator
+
+
+def load_module(shortname):
+    if shortname.startswith("__"):
+        pass
+    elif shortname.endswith("_"):
+        import importlib
+
+        import AnieBot.utils  
+
+        path = Path(f"AnieBot/modules/{shortname}.py")
+        name = "AnieBot.modules.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(mod)
+        print("Successfully imported " + shortname)
+    else:
+        import importlib
+
+        import AnieBot.utils 
+
+        path = Path(f"AnieBot/modules/{shortname}.py")
+        name = "AnieBot.modules.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        mod.Abot = Abot
+        mod.tbot = tbot
+        mod.logger = logging.getLogger(shortname)
+        spec.loader.exec_module(mod)
+        sys.modules["AnieBot.modules." + shortname] = mod
+        print("Successfully imported " + shortname)
+
+
+path = "AnieBot/modules/*.py"
+files = glob.glob(path)
+for name in files:
+    with open(name) as f:
+        path1 = Path(f.name)
+        shortname = path1.stem
+        load_module(shortname.replace(".py", ""))
